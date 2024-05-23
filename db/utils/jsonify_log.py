e3f35ed48f8b530d1aae22713fcbb96d9799c2ac
aradmschool <arad.n@masterschool.com>
2024-05-23 10:39:24 -0400

    processing logs further

--- db/utils/jsonify_log.py
+++ db/utils/jsonify_log.py
@@ -9,10 +9,23 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 openai.api_key = api_key
 
-with open('log_extraction_prompt.md','r') as f:
+with open('utils/log_extraction_prompt.md','r') as f:
     base_prompt = f.read()
 
 base_prompt = base_prompt.replace("\n"," ")
+
+def tag_db(log_json):
+    if len(log_json) == 5:
+        table_name = "System"
+    elif len(log_json) == 3:
+        table_name = "Command"
+    elif len(log_json) == 7:
+        table_name = "Message"
+    else:
+        table_name = "error"
+        print(f"there was a mistake in parsing the log, there's {len(log_json)} fields\n")
+        print(log_json)
+    return table_name
 
 def jsonify_log(log,prompt=base_prompt, model="gpt-4"):
     '''basic completion from gpt4'''
@@ -29,5 +42,7 @@ def jsonify_log(log,prompt=base_prompt, model="gpt-4"):
         output = {}
         print("there was a problem converting to json. empty dictionary returned")
 
-    return output
+    db_name = tag_db(output)
+
+    return {"db_name":db_name, "data":output}
 
